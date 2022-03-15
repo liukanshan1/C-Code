2d7b520cc0e9b769c4d079045ecbc701376acfbe
liukanshan1024/liukanshan1024 <85719360+liukanshan1@users.noreply.github.com>
2022-03-15 20:48:01 +0800

    update

--- C++ 项目/C++ Project/源.cpp
+++ C++ 项目/C++ Project/源.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
-#include<string>
 using namespace std;
-
-
-
-
-
 int main()
 {
-
+	double a, b;
+	char ope;
+	while (cin >> a >> ope >> b)
+	{
+		switch (ope)
+		{
+		case'+':
+			cout << a + b << endl;
+			break;
+		case'-':
+			cout << a - b << endl;
+			break;
+		case'*':
+			cout << a * b << endl;
+			break;
+		case'/':
+			cout << a / b << endl;
+			break;
+		default:
+			break;
+		}
+	}
 }
