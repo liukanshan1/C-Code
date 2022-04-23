ba30809e948bce64d2141d54ce747f9926018e3a
liukanshan1024/liukanshan1024 <85719360+liukanshan1@users.noreply.github.com>
2022-04-23 14:44:35 +0800

    简化E题算法

--- C++ 项目/C++ Project/源1.cpp
+++ C++ 项目/C++ Project/源1.cpp
@@ -1,62 +1,40 @@
 #include<iostream>
 #include<vector>
+#include <string>
+#include <map>
+#include <queue>
 using namespace std;
 
-class node
-{
+class Solution {
 public:
-	int index;
-	node* next = nullptr;
-};
-
-class CircleLinkedList
-{
-public:
-	void pushBack(int n) {
-		node* newNode = new node;
-		newNode->index = n;
-		if (head == nullptr)
+	string frequencySort(string s) {
+		map<char, int> mp;
+		for (auto c : s)
 		{
-			head = newNode;
-			head->next = head;
-			last = head;
-		}
-		else
+			mp[c]++;
+	    }
+		priority_queue<pair<int, char>> pq;
+		for (auto it : mp)
 		{
-			newNode->next = head;
-			last->next = newNode;
-			last = newNode;
+			pq.push(make_pair(it.second, it.first));
 		}
+		string res;
+		while (!pq.empty())
+		{
+			auto p = pq.top();
+			pq.pop();
+			for (int i = 0; i < p.first; i++)
+			{
+				res+=p.second;
+			}
+		}
+		return res;
 	}
-	node* head = nullptr;
-	node* last = nullptr;
 };
 
 int main()
 {
-	int n, m;
-	while (cin >> n >> m)
-	{
-		CircleLinkedList CLL;
-		for (int i = 0; i < n; i++)
-		{
-			CLL.pushBack(i);
-		}
-		node* p = CLL.head;
-		node* pre = CLL.last;	
-		while (p->next != p)
-		{
-			int i = 0;
-			do
-			{
-				p = p->next;
-				pre = pre->next;
-				i++;
-			} while (i != m - 1);
-			pre->next = p->next;
-			delete p;
-			p = pre->next;
-		}
-		cout << p->index << endl;
-	}
+	Solution s;
+	
+	cout << s.frequencySort("tree");
 }
--- 历史文件/2022.04.21上机测试/E.cpp
+++ 历史文件/2022.04.21上机测试/E.cpp
@@ -35,62 +35,47 @@ public:
 		return false;
 	}
 	bool isIsomorphic(String& str) {
+		int len = s.length();
 		if (s.length() != str.s.length())
 		{
 			return false;
 		}
-		int len = s.length();
-		if (len==0)
+		if (len == 0)
 		{
 			return true;
 		}
 		map<char, int> mp1;
 		map<char, int> mp2;
-		auto it1 = mp1.begin();
-		auto it2 = mp2.begin();
-		for (int i = 0; i < len; i++)
+		for (auto c : s)
 		{
-			if ((it1 = mp1.find(s[i]) )!= mp1.end())
-			{
-				mp1[s[i]] += 1;
-			}
-			else
-			{
-				mp1.insert(make_pair(s[i], 1));
-			}
-			if ((it2 = mp2.find(str.s[i]))!= mp2.end())
-			{
-				mp2[str.s[i]] += 1;
-			}
-			else
-			{
-				mp2.insert(make_pair(str.s[i], 1));
-			}
+			mp1[c]++;
+		}
+		for (auto c : str.s)
+		{
+			mp2[c]++;
 		}
 		priority_queue<int> q1, q2;
-		for (auto it = mp1.begin();it!=mp1.end();it++)
+		for (auto it : mp1)
 		{
-			q1.push(it->second);
+			q1.push(it.second);
 		}
-		for (auto it = mp2.begin(); it != mp2.end(); it++)
+		for (auto it : mp2)
 		{
-			q2.push(it->second);
+			q2.push(it.second);
 		}
+
 		if (q1.size()!=q2.size())
 		{
 			return false;
 		}
 		while (q1.size()!=0)
 		{
-			int a, b;
-			a = q1.top();
-			b = q2.top();
-			q1.pop();
-			q2.pop();
-			if (a!=b)
+			if (q1.top() != q2.top())
 			{
 				return false;
 			}
+			q1.pop();
+			q2.pop();
 		}
 		return true;
 	}
